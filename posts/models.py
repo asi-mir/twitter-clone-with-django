77307e60986b76d79fabe1d3ef9cc969378c5d49
hussain <hhh.hosein.619@gmail.com>
2021-02-28 11:29:25 +0330

    Post model, FileField , Tags Added

--- posts/models.py
+++ posts/models.py
@@ -1,14 +1,20 @@
 from django.db import models
 from accounts.models import Profile
+from taggit.managers import TaggableManager
 
 
 class Post(models.Model):
-    author = models.ForeignKey(Profile, on_delete=models.DO_NOTHING, null=False, related_name='author')
-    picture = models.ImageField(upload_to='images', blank=True)
+    author = models.ForeignKey(Profile, on_delete=models.CASCADE, null=False, related_name='author')
+
     body = models.TextField()
+
     liked = models.ManyToManyField(Profile, default=None, blank=True, related_name='liked')
+
     creat_date = models.DateTimeField(auto_now_add=True)
+
     parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=type)
+
+    tags = TaggableManager()
 
     def __str__(self):
         return str(self.body)
@@ -20,3 +26,11 @@ class Post(models.Model):
     @property
     def like_count(self):
         return self.liked.all().count()
+
+
+class Files(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    files = models.FileField(upload_to='images', null=True, blank=True)
+
+    def __str__(self):
+        return self.post.body + 'File'
