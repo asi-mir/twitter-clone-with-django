108bfcfda225bc1b63a74928aa51b8a695f48b1c
CandySweet997 <70762909+CandySweet997@users.noreply.github.com>
2021-03-09 01:19:36 +0330

    media(default pic)

--- accounts/models.py
+++ accounts/models.py
@@ -2,6 +2,8 @@ from django import forms
 from django.db import models
 from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.utils import timezone
+
+
 class UserManager(BaseUserManager):
     def create_user(self, phone, password=None, **other_fields):
         if not phone:
@@ -39,7 +41,6 @@ class User(AbstractUser):
 
 
 class Profile(models.Model):
-
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     first_name = models.CharField(max_length=30, default="")
     last_name = models.CharField(max_length=30, blank=True, default="")
@@ -61,7 +62,7 @@ class Profile(models.Model):
     account_type = models.CharField(max_length=6, choices=ACCOUNT_TYPE_CHOICES, default='')
 
     def __str__(self):
-          return f'{self.user.phone}'
+        return f'{self.user.phone}'
 
     @property
     def followers(self):
