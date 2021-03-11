14e875983b01ddf10482aaee30fb87412e6ef98e
farhadkazemian <79379734+farhadkazemian@users.noreply.github.com>
2021-03-11 19:59:28 +0330

    Unlike is added

--- posts/views.py
+++ posts/views.py
@@ -9,7 +9,14 @@ from .models import Post
 
 def LikeView(request,pk):
     post=get_object_or_404(Post,id=request.POST.get('post_id'))
-    post.likes.add(request.user)
+    liked = False
+    if post.likes.filter(id=request.user.id).exists():
+        post.likes.remove(request.user)
+        liked=False
+    else:
+        post.likes.add(request.user)
+        liked=True
+
     return HttpResponseRedirect(reverse('posts:home'))
 
 class TweetListView(LoginRequiredMixin, ListView):
