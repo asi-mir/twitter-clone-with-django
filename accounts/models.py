ac7c417346db82f2453508f55c47b29d9f0d0f1c
Ebrahim Barati Nasab <abrahim_barati@yahoo.com>
2021-03-17 19:33:12 +0330

    Login phone validation

--- accounts/models.py
+++ accounts/models.py
@@ -27,7 +27,10 @@ class UserManager(BaseUserManager):
 
 class User(AbstractUser):
     username = None
-    phone = models.CharField(max_length=11, unique=True)
+    phone_regex = RegexValidator(regex = r'^[0][9]\d{9}$|^[1-9]\d{9}$',
+        message = 'Phone number up to 11 digits allowed start with 09')
+
+    phone = models.CharField(validators = [phone_regex], max_length=11, unique=True)
     token = models.CharField(max_length=64, blank=True, null=True)
     token_expiration_date = models.DateTimeField(null=True)
     salt = models.CharField(max_length=32, null=True)
